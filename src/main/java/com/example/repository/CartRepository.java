4f984540aaa8ff461ff920e1a2fe89597f887c7c
jiinyoo <yji3791@gmail.com>
2024-04-26 18:01:59 +0900

    first commit

---
+++ src/main/java/com/example/repository/CartRepository.java
@@ -0,0 +1,13 @@
+package com.example.repository;
+
+
+import com.example.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+    Cart findByMemberId(Long memberId);
+}
+
+
